2bd6a030663ecb818f756723f91f3c9330ad8a90
Hamza Asghar <hamzaraftar765@gmail.com>
2025-04-01 10:20:27 +0500

    full stack todo app with auth and CRUD feature

---
+++ frontend/src/components/LoadingIndicator.jsx
@@ -0,0 +1,7 @@
+export default function LoadingIndicator() {
+  return (
+    <div className="flex justify-center">
+      <div className="w-6 h-6 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+    </div>
+  );
+}
---
+++ frontend/src/pages/Todos.jsx
@@ -0,0 +1,11 @@
+import TodoHeader from "../components/TodoHeader";
+import TodoInput from "../components/TodoInput";
+
+export default function Todos() {
+  return (
+    <div >
+      <TodoHeader />
+      <TodoInput />
+    </div>
+  );
+}
